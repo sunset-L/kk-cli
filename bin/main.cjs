4edfd4e1e05d1c0180176cf14f9fd7b2e35bb309
刘欢 <sunset0827@163.com>
2023-07-20 15:58:25 +0800

    feat 代码梳理优化&增加对话

--- bin/main.cjs
+++ bin/main.cjs
@@ -1,7 +1,5 @@
 #! /usr/bin/env node
 const program = require("commander");
-const chalk = require("chalk");
-const ora = require("ora");
 
 program
   .name('kk-cli')
@@ -12,9 +10,10 @@ program
   .command("create <project-name>") // 增加创建指令
   .description("create a new project") // 添加描述信息
   .option("-f, --force", "overwrite target directory if it exists") // 强制覆盖
-  .action((projectName, cmd) => {
-    // 处理用户输入create 指令附加的参数
-    require('../lib/create')(projectName, cmd)
+  .action(async (projectName, cmd) => {
+    const opt = await require('../lib/prompts')(projectName, cmd)
+    if (opt.cancel) return
+    require('../lib/create')(projectName, opt)
   });
 
 
